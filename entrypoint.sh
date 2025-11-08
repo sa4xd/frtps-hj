e67b7564c1566c0738ff9af81181f300923da9fa
sa4xd <xxuhjx@mkij8jjo08.ip-ddns.com>
2025-11-08 08:50:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 cat <<EOF > /opt/frps.toml
-bindPort = ${FRPS_BIND_PORT}
+bindPort = ${FRPS_BIND_PORT:-7000}
 
 [auth]
-method = "${FRPS_AUTH_METHOD}"
-token = "${FRPS_AUTH_TOKEN}"
+method = "${FRPS_AUTH_METHOD:-token}"
+token = "${FRPS_AUTH_TOKEN:-frp-default-token}"
 
 [webServer]
-addr = "${FRPS_WEB_SERVER_ADDR}"
-port = ${FRPS_WEB_SERVER_PORT}
-user = "${FRPS_WEB_SERVER_USER}"
-password = "${FRPS_WEB_SERVER_PASSWORD}"
+addr = "${FRPS_WEB_SERVER_ADDR:-0.0.0.0}"
+port = ${FRPS_WEB_SERVER_PORT:-7500}
+user = "${FRPS_WEB_SERVER_USER:-admin}"
+password = "${FRPS_WEB_SERVER_PASSWORD:-123456}"
 EOF
 
 exec frps -c /opt/frps.toml
